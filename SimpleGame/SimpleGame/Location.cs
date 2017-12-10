a50a3236460d9dc774faf9ab978864b5129e6c11
DobromirAtanasov <30621566+DobromirAtanasov@users.noreply.github.com>
2017-12-10 22:31:51 +0200

    class Location

---
+++ SimpleGame/SimpleGame/Location.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGame
+{
+    //This class should take care of object Current Location as well as Changing it (looking for borders, gates etc.)
+    class Location
+    {
+        private int xCoordiante;
+        private int yCoordinate;
+        public int XCoordinate
+        {
+            get { return this.xCoordiante; }
+            set
+            {
+                if (value <= 0 || value >= 127)
+                {
+                    throw new ArgumentException("Border reached");
+                }
+                this.xCoordiante = value;
+            }
+        }
+        public int YCoordinate { get { return this.yCoordinate; } set {
+                if (value<=0||value>=80)
+                {
+                    throw new ArgumentException("Border reached");
+                }
+                this.yCoordinate = value; } }
+        public Location(int xCoordinate, int yCoordinate)
+        {
+
+        }
+    }
+}
---
+++ SimpleGame/SimpleGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Some testing. All must be moved to method Initialize Console.
+            {
+                System.Console.SetWindowSize(128, 70);
+                System.Console.Title = "SimpleGame";
+                Console.BufferHeight = 70;
+
+                for (int i = 0; i < 70; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
